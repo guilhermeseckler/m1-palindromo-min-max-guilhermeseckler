2fe349f3d507c60cf33002168fb3cf53c9ae1ec3
Guilherme Seckler <guilhermeseckler@outlook.com>
2024-05-28 20:03:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-function isPalindrome(str){
-    /* Seu código aqui */
-}
-
-function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+function isPalindrome (str) {
+    let reversedStr = ''
+    let cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversedStr += str[i]
+      reversedStr = reversedStr.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    }
+    if (reversedStr === cleanStr) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+  
+function arrayMaxMin (arr){
+    let arrMin = arr[0];
+    let arrMax = 0;
+  
+    for (let i = 0; i <= arr.length; i++) {
+      if (arr[i] > arrMax) {
+        arrMax = arr[i];
+      }
+      if (arr[i] < arrMin) {
+        arrMin = arr[i];  
+      }
+    }
+    return [arrMin, arrMax]
+  }
